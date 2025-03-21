95f9802aeca8c83ae6cce17a198bbb6feba62a21
NISHANT-593 <nishant0593.becse24@chitkara.edu.in>
2025-03-21 15:06:30 +0530

    Solution to Logical AND Operation

--- Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -2,6 +2,15 @@
 int main(){
     int num1 , num2 ;
     scanf("%d %d ",&num1 ,&num2);
-    printf((num1 >= 0 && num2 >= 0)? "True\n":"False");
+    
+    if(num1>=0&&num2>=0){
+        printf("True\n");
+    }
+    
+
+    else{
+        printf("Flase\n");
+    }
+
     return 0;
 } 
